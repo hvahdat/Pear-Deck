3c68832c8283503a28dc3da537910728e696a5ca
hvahdat <42591321+hvahdat@users.noreply.github.com>
2019-10-29 16:48:46 -0500

    Update Use R to pull from BigQuery.R

--- Use R to pull from BigQuery.R
+++ Use R to pull from BigQuery.R
@@ -10,4 +10,4 @@ project_id <- "peardeck-external-projects"
 
 sql_string <- "SELECT * FROM `peardeck-external-projects.buisness_analytics_in_practice_project.app_events` LIMIT 500000"
 
-query_results <- query_exec(sql_string, project = project_id, use_legacy_sql = FALSE)
+query_results <- query_exec(sql_string, project = project_id, use_legacy_sql = FALSE, max_pages = Inf)
