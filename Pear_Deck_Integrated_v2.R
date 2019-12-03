1e8d8c805fad9a5235805c21a422377d6d9f8dba
hvahdat <42591321+hvahdat@users.noreply.github.com>
2019-12-02 19:42:23 -0600

    Update Pear_Deck_Integrated_v2.R

--- Pear_Deck_Integrated_v2.R
+++ Pear_Deck_Integrated_v2.R
@@ -1253,12 +1253,12 @@ df_sr$num_students[is.na(df_sr$num_students)] <- 0
 
 # Create bucket labels for num_students
 df_sr$num_students_label <- df_sr$num_students
-df_sr$num_students_label[which(df_sr$num_students == 0)] <- "0"
-df_sr$num_students_label[which(df_sr$num_students == 1)] <- "1"
-df_sr$num_students_label[which(df_sr$num_students >= 2 & df_sr$num_students <= 10)] <- "2-10"
-df_sr$num_students_label[which(df_sr$num_students >= 11 & df_sr$num_students <= 19)] <- "11-19"
-df_sr$num_students_label[which(df_sr$num_students >= 20 & df_sr$num_students <= 30)] <- "20-30"
-df_sr$num_students_label[which(df_sr$num_students >= 31)] <- "31+"
+df_sr$num_students_label[which(df_sr$num_students == 0)] <- "0_Testing"
+df_sr$num_students_label[which(df_sr$num_students == 1)] <- "1_Low"
+df_sr$num_students_label[which(df_sr$num_students >= 2 & df_sr$num_students <= 10)] <- "2_Low-Medium"
+df_sr$num_students_label[which(df_sr$num_students >= 11 & df_sr$num_students <= 19)] <- "3_Medium"
+df_sr$num_students_label[which(df_sr$num_students >= 20 & df_sr$num_students <= 30)] <- "4_Medium-High"
+df_sr$num_students_label[which(df_sr$num_students >= 31)] <- "5_High"
 
 # Rename the levels (note: did it this way to preserve the desired order)
 df_sr$num_students_label <- factor(df_sr$num_students_label)
